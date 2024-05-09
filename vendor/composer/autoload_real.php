4932266852b1e04fb6de0b00c0ffbc8da3067fd5
Rodrigo Arias <rodrigo@arias.uy>
2024-05-09 20:23:29 -0300

    fix: replace deprecated string interpolation

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit7096d5cfc4b7fd4d0b570502eac8d255
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit7096d5cfc4b7fd4d0b570502eac8d255::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire7096d5cfc4b7fd4d0b570502eac8d255($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit7096d5cfc4b7fd4d0b570502eac8d255::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire7096d5cfc4b7fd4d0b570502eac8d255($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
